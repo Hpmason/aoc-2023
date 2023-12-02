153d61375c128475860cbe7877c5f8d0fbbd361b
Mason Ginter <mason@dagint.com>
2023-12-02 18:53:38 -0500

    Day 2 Part 2: fully working

--- cmd/day2/day2.go
+++ cmd/day2/day2.go
@@ -11,28 +11,13 @@ import (
 func main() {
 	const numWorkers = 10
 	var maxPull = pull{12, 13, 14}
-	dat, err := os.ReadFile("data/day2-ex1.txt")
+	dat, err := os.ReadFile("data/day2.txt")
 	if err != nil {
 		panic(err)
 	}
 	text := string(dat)
 	lines := strings.Split(text, "\n")
-	// for _, line := range lines {
-	// 	if len(line) == 0 {
-	// 		continue
-	// 	}
-	// 	game, err := parseGame(line)
-	// 	if err != nil {
-	// 		for i := range line {
-	// 			fmt.Println(line[i])
-	// 		}
-	// 		fmt.Println("Error parsing: ", err)
-	// 	} else {
-	// 		fmt.Println(game)
-	// 	}
-	// }
-	
-	
+
 	jobs := make(chan string, len(lines))
 	results := make(chan int, len(lines))
 
@@ -40,9 +25,13 @@ func main() {
 	for w := 1; w <= numWorkers; w++ {
 		go worker(jobs, results, maxPull)
 	}
+
+	skipCount := 0
 	// Queue up jobs
 	for _, line := range lines {
+		// Deal with blank lines
 		if len(line) == 0 {
+			skipCount += 1
 			continue
 		}
 		jobs <- line
@@ -51,10 +40,37 @@ func main() {
 
 	// Reduce results
 	result := 0
-	for r := 1; r <= len(lines) - 1; r++ {
+	for r := 1; r <= len(lines) - skipCount; r++ {
 		result += <-results
 	}
 	fmt.Println("Result: ", result)
+}
+
+func calc(lines []string) {
+	result := 0
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue;
+		}
+		game, err := parseGame(line)
+		if err != nil {
+			panic(err)
+		}
+		pull := game.getMinPull()
+		result += pull.product()
+	}
+	fmt.Println("Results: ", result)
+}
+
+func worker(jobs <-chan string, results chan<- int, maxPull pull) {
+	for line := range jobs {
+		game, err := parseGame(line)
+		if err != nil {
+			panic(err)
+		}
+		pull := game.getMinPull()
+		results <- pull.product()
+	}
 }
 
 type game struct {
@@ -65,7 +81,6 @@ type game struct {
 type pull struct {
 	red, green, blue int
 }
-
 
 func parseGame(line string) (game, error) {
 	game := game{}
@@ -99,11 +114,13 @@ func parseGame(line string) (game, error) {
 			}
 			switch color {
 			case "red":
-				pull.red = int(count)
+				pull.red += int(count)
 			case "green":
-				pull.green = int(count)
+				pull.green += int(count)
 			case "blue":
-				pull.blue = int(count)
+				pull.blue += int(count)
+			default:
+				return game, errors.New("Unexpected color: \"" + color + "\"")
 			}
 		}
 		pulls[i] = pull
@@ -112,43 +129,40 @@ func parseGame(line string) (game, error) {
 	return game, nil
 }
 
-func (g *game) isValid(maxPull pull) bool {
-	cumulativePull := pull {}
+func (g *game) getMinPull() pull {
+	minPull := pull {}
 	for _, pull := range g.pulls {
-		if pull.red > cumulativePull.red {
-			cumulativePull.red = pull.red
-		}
-		if pull.green > cumulativePull.green {
-			cumulativePull.green = pull.green
-		}
-		if pull.blue > cumulativePull.blue {
-			cumulativePull.blue = pull.blue
-		}
+		minPull.red = max(minPull.red, pull.red)
+		minPull.green = max(minPull.green, pull.green)
+		minPull.blue = max(minPull.blue, pull.blue)
 	}
+	return minPull
+}
 
-	if cumulativePull.red > maxPull.red {
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func (g *game) isValid(maxPull pull) bool {
+	minPull := g.getMinPull()
+	if minPull.red > maxPull.red {
 		return false
 	}
-	if cumulativePull.green > maxPull.green {
+	if minPull.green > maxPull.green {
 		return false
 	}
-	if cumulativePull.blue > maxPull.blue {
+	if minPull.blue > maxPull.blue {
 		return false
 	}
 	return true
 }
 
-func worker(jobs <-chan string, results chan<- int, maxPull pull) {
-	for line := range jobs {
-		game, err := parseGame(line)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(game)
-		if game.isValid(maxPull) {
-			results <- game.num
-		} else {
-			results <- 0
-		}
-	}
+func (p *pull) product() int {
+	return p.red * p.green * p.blue
 }
+
+
