713bbf8003d305d0cb7786e64f8f9475e0f49af5
Mason Ginter <mason@dagint.com>
2023-12-02 18:57:24 -0500

    Day 2: cleanup

--- cmd/day2/day2.go
+++ cmd/day2/day2.go
@@ -46,22 +46,6 @@ func main() {
 	fmt.Println("Result: ", result)
 }
 
-func calc(lines []string) {
-	result := 0
-	for _, line := range lines {
-		if len(line) == 0 {
-			continue;
-		}
-		game, err := parseGame(line)
-		if err != nil {
-			panic(err)
-		}
-		pull := game.getMinPull()
-		result += pull.product()
-	}
-	fmt.Println("Results: ", result)
-}
-
 func worker(jobs <-chan string, results chan<- int, maxPull pull) {
 	for line := range jobs {
 		game, err := parseGame(line)
@@ -164,5 +148,3 @@ func (g *game) isValid(maxPull pull) bool {
 func (p *pull) product() int {
 	return p.red * p.green * p.blue
 }
-
-
